7b535ce15e81914526a2d5d6bb9d9a394a5d470a
nguyen xuan bang <ngxuanbang2207@mail.com>
2023-06-14 08:42:36 +0700

    first commit

---
+++ EmguCvDetectFace/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace EmguCvDetectFace
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.picImageFace = new System.Windows.Forms.PictureBox();
+            this.picDetectFace = new System.Windows.Forms.PictureBox();
+            this.btnBrowser = new System.Windows.Forms.Button();
+            this.btnDetect = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.picImageFace)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picDetectFace)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picImageFace
+            // 
+            this.picImageFace.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.picImageFace.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.picImageFace.Location = new System.Drawing.Point(-5, 46);
+            this.picImageFace.Name = "picImageFace";
+            this.picImageFace.Size = new System.Drawing.Size(415, 231);
+            this.picImageFace.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picImageFace.TabIndex = 0;
+            this.picImageFace.TabStop = false;
+            this.picImageFace.Click += new System.EventHandler(this.picImageFace_Click);
+            // 
+            // picDetectFace
+            // 
+            this.picDetectFace.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.picDetectFace.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.picDetectFace.Location = new System.Drawing.Point(432, 46);
+            this.picDetectFace.Name = "picDetectFace";
+            this.picDetectFace.Size = new System.Drawing.Size(356, 231);
+            this.picDetectFace.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picDetectFace.TabIndex = 0;
+            this.picDetectFace.TabStop = false;
+            // 
+            // btnBrowser
+            // 
+            this.btnBrowser.Location = new System.Drawing.Point(-5, 17);
+            this.btnBrowser.Name = "btnBrowser";
+            this.btnBrowser.Size = new System.Drawing.Size(75, 23);
+            this.btnBrowser.TabIndex = 1;
+            this.btnBrowser.Text = "Browser";
+            this.btnBrowser.UseVisualStyleBackColor = true;
+            this.btnBrowser.Click += new System.EventHandler(this.btnBrowser_Click);
+            // 
+            // btnDetect
+            // 
+            this.btnDetect.Location = new System.Drawing.Point(258, 17);
+            this.btnDetect.Name = "btnDetect";
+            this.btnDetect.Size = new System.Drawing.Size(75, 23);
+            this.btnDetect.TabIndex = 2;
+            this.btnDetect.Text = "Detect";
+            this.btnDetect.UseVisualStyleBackColor = true;
+            this.btnDetect.Click += new System.EventHandler(this.btnDetect_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnDetect);
+            this.Controls.Add(this.btnBrowser);
+            this.Controls.Add(this.picDetectFace);
+            this.Controls.Add(this.picImageFace);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.picImageFace)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picDetectFace)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picImageFace;
+        private System.Windows.Forms.PictureBox picDetectFace;
+        private System.Windows.Forms.Button btnBrowser;
+        private System.Windows.Forms.Button btnDetect;
+    }
+}
+
---
+++ EmguCvDetectFace/Form1.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
+using Emgu.CV;
+using Emgu.CV.Structure;
+
+namespace EmguCvDetectFace
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Image<Bgr, Byte> ImageFrame;
+        private CascadeClassifier _cascadeClassifier = new CascadeClassifier("haarcascade_frontalface_alt_tree.xml");
+        Image InputImg;
+        string filePath = "";
+
+        private void picImageFace_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnBrowser_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                OpenFileDialog openFile = new OpenFileDialog();
+                if (openFile.ShowDialog() == DialogResult.OK)
+                {
+                    filePath = openFile.FileName;
+                    string fileName = Path.GetFileName(openFile.FileName);
+                    //InputImg = Image.FromFile(openFile.FileName);
+                    InputImg = AutoResizeImage(openFile.FileName);
+                    //ImageFrame = new Image<Bgr, byte>(openFile.FileName);
+                    //ImageFrame = new Image<Bgr, byte>(new Bitmap(InputImg));
+                    ImageFrame = new Bitmap(InputImg).ToImage<Bgr,byte>();
+                    picImageFace.Image = ImageFrame.AsBitmap();
+                }
+                else
+                {
+                    MessageBox.Show("No file selected.");
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        public Image AutoResizeImage(string url)
+        {
+            var InputImg = Image.FromFile(url);
+            ImageFrame = new Bitmap(InputImg).ToImage<Bgr, byte>();
+            Image<Gray, byte> grayframe = ImageFrame.Convert<Gray, byte>();
+            var faces = _cascadeClassifier.DetectMultiScale(grayframe, 1.1, 10, Size.Empty);
+
+            if (faces.Length > 0)
+            {
+                Bitmap BmpInput = grayframe.ToBitmap();
+                Bitmap ExtractedFace;
+                Graphics FaceCanvas;
+
+                foreach (var face in faces)
+                {
+                    ImageFrame.Draw(face, new Bgr(Color.Blue), 4);
+                    ExtractedFace = new Bitmap(face.Width, face.Height);
+                    FaceCanvas = Graphics.FromImage(ExtractedFace);
+
+                    FaceCanvas.DrawImage(BmpInput, 0, 0, face, GraphicsUnit.Pixel);
+                    int w = face.Width;
+                    int h = face.Height;
+                    int x = face.X;
+                    int y = face.Y;
+
+                    int r = Math.Max(250, 250) / 2;
+                    int centerx = x + w / 2;
+                    int centery = y + h / 2;
+                    int nx = (int)(centerx - r);
+                    int ny = (int)(centery - r);
+                    int nr = (int)(r * 5);
+
+
+                    double zoomFactor = (double)197 / (double)face.Width;
+                    Size newSize = new Size((int)(InputImg.Width * zoomFactor), (int)(InputImg.Height * zoomFactor));
+                    Bitmap bmp = new Bitmap(InputImg, newSize);
+                    return (Image)bmp;
+                }
+
+
+            }
+            return InputImg;
+        }
+
+        private void DetectFaces()
+        {
+            try
+            {
+                Image<Gray, byte> grayframe = ImageFrame.Convert<Gray, byte>().Clone();
+                _cascadeClassifier = new CascadeClassifier("haarcascade_frontalface_default.xml");
+                Rectangle[] faces = _cascadeClassifier.DetectMultiScale(grayframe, 1.1, 10, Size.Empty);
+                if (faces.Length > 0)
+                {
+                    Bitmap BmpInput = grayframe.ToBitmap();
+                    Bitmap ExtractedFace;
+                    Graphics FaceCanvas;
+
+                    foreach (var face in faces)
+                    {
+                        ImageFrame.Draw(face, new Bgr(Color.Blue), 4);
+                        ExtractedFace = new Bitmap(face.Width, face.Height);
+                        FaceCanvas = Graphics.FromImage(ExtractedFace);
+                        FaceCanvas.DrawImage(BmpInput, 0, 0, face, GraphicsUnit.Pixel);
+                        if (face.Width < 100) { return; }
+                        int w = face.Width;
+                        int h = face.Height;
+                        int x = face.X;
+                        int y = face.Y;
+
+                        int r = Math.Max(250, 250) / 2;
+                        int centerx = x + w / 2;
+                        int centery = y + h / 2;
+                        int nx = (int)(centerx - r);
+                        int ny = (int)(centery - r);
+                        int nr = (int)(r * 5);
+
+
+                        double zoomFactor = (double)197 / (double)face.Width;
+                        Size newSize = new Size((int)(InputImg.Width * zoomFactor), (int)(InputImg.Height * zoomFactor));
+                        Bitmap bmp = new Bitmap(InputImg, newSize);
+                        picDetectFace.Image = (Image)bmp;
+                        var imgextract = CropImage(picDetectFace.Image, nx + 4, ny - 25, 248, 340);
+                        picDetectFace.Image = imgextract;
+                    }
+
+                    picImageFace.Image = ImageFrame.AsBitmap();
+
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        public  Bitmap CropImage(Image source, int x, int y, int width, int height)
+        {
+            Rectangle crop = new Rectangle(x, y, width, height);
+            string cropFileName = "";
+            string cropFilePath = "";
+            string fileName = ""; 
+            if (File.Exists(filePath))
+            {
+                 fileName = Path.GetFileName(filePath);
+
+            }
+            var bmp = new Bitmap(crop.Width, crop.Height);
+            using (var gr = Graphics.FromImage(bmp))
+            {
+                gr.DrawImage(source, new Rectangle(0, 0, bmp.Width, bmp.Height), crop, GraphicsUnit.Pixel);
+            }
+            //cropFileName = "crop_" + fileName;
+            //cropFilePath = Path.Combine(Path.GetDirectoryName(filePath),"DetectCrop", cropFileName);
+            string folderCropPath = Path.Combine(Path.GetDirectoryName(filePath), "DetectCrop");
+            if (!Directory.Exists(folderCropPath))
+            {
+                Directory.CreateDirectory(folderCropPath);
+            }
+            cropFilePath = Path.Combine(folderCropPath, fileName);
+            bmp.Save(cropFilePath);
+            return bmp;
+        }
+
+        private void btnDetect_Click(object sender, EventArgs e)
+        {
+            DetectFaces();
+        }
+    }
+}
